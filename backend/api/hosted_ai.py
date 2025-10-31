bea2c1bc35b58a7c6595646a20bcb8b6177b5677
Daylen Smith <daylencsmith@gmail.com>
2025-10-30 22:13:44 -0700

    add test workflow

--- backend/api/hosted_ai.py
+++ backend/api/hosted_ai.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import FastAPI, Request, UploadFile, File, APIRouter
+from pathlib import Path
+
 
 router = APIRouter(
     prefix="/ai",
@@ -8,3 +10,23 @@ router = APIRouter(
 @router.get("/")
 def test():
     return {"message": "ai"}
+
+
+@router.post("/summarize/")
+async def summarize_video(request: Request, file: UploadFile = File(...)):
+    # Access the model
+    ai_model = request.app.state.ai_model
+
+    # Save the uploaded file
+    uploads_dir = Path("uploads")
+    uploads_dir.mkdir(exist_ok=True)
+    video_path = uploads_dir / file.filename
+
+    with open(video_path, "wb") as f:
+        f.write(await file.read())
+    print(video_path)
+
+    # Run inference
+    summary = ai_model.summarize_video(str(video_path))
+    print(summary)
+    return {"summary": summary}
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+import torch
 
 from api import admin_router, user_router, ai_router
+from ai.ai_class import SmolVLM2Wrapper
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    app.state.ai_model = SmolVLM2Wrapper(
+        model_path="HuggingFaceTB/SmolVLM2-256M-Video-Instruct",
+        device="cpu",
+        dtype=torch.float32
+    )
+    print("AI model loaded!")
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins for testing
+    allow_methods=["*"],  # allow POST, GET, etc.
+    allow_headers=["*"],  # allow custom headers
+)
+
 app.include_router(admin_router)
 app.include_router(user_router)
 app.include_router(ai_router)
