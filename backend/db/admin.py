8ce542c24f57af13dd1bd061af6cdbb196119a97
izcheung <icheung24@my.bcit.ca>
2025-11-01 13:09:26 -0700

    feat: add user and admin db operations

---
+++ backend/db/admin.py
@@ -0,0 +1,29 @@
+import psycopg
+from db.connection import get_db_connection
+
+
+def insert_admin(email: str, hashed_password: str):
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO admin (email, password)
+                    VALUES (%s, %s)
+                    RETURNING id, email
+                    """,
+                    (email, hashed_password),
+                ) 
+                return cur.fetchone()
+    except psycopg.errors.UniqueViolation:
+        return None
+
+
+def get_admin_by_email(email: str):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT id, password FROM admin WHERE email = %s",
+                (email,),
+            )
+            return cur.fetchone()
---
+++ backend/db/user.py
@@ -0,0 +1,29 @@
+import psycopg
+from db.connection import get_db_connection
+
+
+def insert_user(email: str, hashed_password: str):
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO 'user' (email, password)
+                    VALUES (%s, %s)
+                    RETURNING id, email, api_requests_left
+                    """,
+                    (email, hashed_password),
+                )
+                return cur.fetchone()
+    except psycopg.errors.UniqueViolation:
+        return None
+
+
+def get_user_by_email(email: str):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                'SELECT id, password, api_requests_left FROM "user" WHERE email = %s',
+                (email,),
+            )
+            return cur.fetchone()
