dcbbc5dcaeddff41a69ad156b38ee813c0a15606
izcheung <icheung24@my.bcit.ca>
2025-11-01 13:24:28 -0700

    feat: add db function to update number of api calls left

--- backend/db/user.py
+++ backend/db/user.py
@@ -27,3 +27,23 @@ def get_user_by_email(email: str):
                 (email,),
             )
             return cur.fetchone()
+
+
+def get_user_by_id(user_id: int):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                'SELECT id, email, api_requests_left FROM "user" WHERE id = %s',
+                (user_id,),
+            )
+            return cur.fetchone()
+
+
+def update_user_api_requests_left(user_id: int, amount: int):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                'UPDATE "user" SET api_requests_left = api_requests_left - %s WHERE id = %s',
+                (amount, user_id),
+            )
+            conn.commit()
