b33b58ed8a2ddd92e81c486ed561f71d095ef0d8
elenajou <elenajou.ej@gmail.com>
2025-11-02 21:35:24 -0800

    fixed backend url

--- frontend/scripts/admin_landing.js
+++ frontend/scripts/admin_landing.js
@@ -1,4 +1,4 @@
-import BACKEND_URL from "./config";
+import { BACKEND_URL } from "./config.js";
 
 document.getElementById("greeting").textContent = "Admin Dashboard";
 
--- frontend/scripts/register_customer.js
+++ frontend/scripts/register_customer.js
@@ -1,4 +1,4 @@
-import BACKEND_URL from "./config";
+import { BACKEND_URL } from "./config.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById('registrationForm');
