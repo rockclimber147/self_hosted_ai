09459af855a6ec914af09aed69e4934f73c89f0c
izcheung <icheung24@my.bcit.ca>
2025-11-25 09:43:55 -0800

    feat: add lang

---
+++ backend/lang/en/messages.py
@@ -0,0 +1,22 @@
+DOM_MESSAGES = {
+    "customer": {
+        "defaultResponse": "No response yet.",
+        "errorLoadUser": "Failed to fetch user list",
+        "alert": "Please select a video file first.",
+        "serverError": "Server error: ",
+        "uploadError": "Error uploading video: ",
+        "invalidFormat": "Invalid file format. Only MP4 is allowed.",
+        "invalidMine": "Invalid file type. File must be a valid MP4 video.",
+    },
+    "admin": {
+        "fetchError": "Failed to fetch user list",
+        "h2Endpoint": "API Stats Overview",
+        "loadingError": "Error loading endpoints",
+        "endpointError": "No endpoints found",
+        "loadingEndpoint": "Loading endpoints...",
+        "colMethod": "Method",
+        "colEndpoint": "Endpoint",
+        "colEndpointRequests": "Request Count",
+        "logoutFailed": "Logout failed:",
+    },
+}
